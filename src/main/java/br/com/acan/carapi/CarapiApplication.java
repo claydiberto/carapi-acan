a08391d43d7bd83d313ad4eee005e2a8a2a9eb28
Antonio Claydiberto <claydiberto@gmail.com>
2020-12-04 11:36:13 -0300

    Add Project to Git
    First commit

---
+++ src/main/java/br/com/acan/carapi/CarapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.acan.carapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/acan/carapi/CarapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.acan.carapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
